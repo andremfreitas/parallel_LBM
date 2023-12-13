c5fa9bd2f376a5c55b68c243f05304e6baf3d3d2
elbellantoni <e.bellantoni@cyi.ac.cy>
2023-12-13 18:09:46 +0100

    Plot for strong scaling

---
+++ openmp/sscaling-plot.py
@@ -0,0 +1,79 @@
+# Plot for strong scaling
+from matplotlib import pyplot as plt
+import numpy as np
+
+case = "output/nx64_ny64_nz64"
+
+nx = ny = nz = 64
+
+data = np.loadtxt(case + ".txt", usecols=(2,5,9)) # Get data for time, speed and thread number
+
+nthr  = np.unique(data[:,2])
+
+s0 = []
+s1 = []
+s2 = []
+s3 = []
+s4 = []
+
+for i in range(data.shape[0]):
+    if   data[i,2] == nthr[0]:
+        s0.append(data[i,1])
+        
+    elif data[i,2] == nthr[1]:
+        s1.append(data[i,1])
+        
+    elif data[i,2] == nthr[2]:
+        s2.append(data[i,1])
+        
+    elif data[i,2] == nthr[3]:
+        s3.append(data[i,1])
+        
+    elif data[i,2] == nthr[4]:
+        s4.append(data[i,1])
+
+speed    = np.array([
+    np.average(s0[1:]),
+    np.average(s1[1:]),
+    np.average(s2[1:]),
+    np.average(s3[1:]),
+    np.average(s4[1:])
+])
+
+speed_err = np.array([
+    np.std(s0[1:])/np.sqrt(len(s0[1:])-1),
+    np.std(s1[1:])/np.sqrt(len(s1[1:])-1),
+    np.std(s2[1:])/np.sqrt(len(s2[1:])-1),
+    np.std(s3[1:])/np.sqrt(len(s3[1:])-1),
+    np.std(s4[1:])/np.sqrt(len(s4[1:])-1)
+])
+
+# LaTeX font selection
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "STIXGeneral",
+})
+
+# Common plot params
+figsize = (10,10)
+labelsize  = 18 + 4
+titlesize  = 20 + 4
+legendsize = 16 + 4
+ticksize   = 16 + 4
+
+plotname = case + ".pdf"
+
+fig, ax = plt.subplots(figsize=figsize)
+
+# ax.plot(nthr, speed, "o-", label=r"64x64x64 Grid" )
+ax.errorbar(nthr, speed, speed_err, elinewidth=2, fmt=".-", label=r"64x64x64 Grid", c="tab:purple" )
+
+ax.tick_params(axis = "both", labelsize = ticksize)
+ax.set_ylabel(r"Speed [MLUP/s]", fontsize = labelsize)
+ax.set_xlabel(r"$N_{\mathrm{thr}}$", fontsize = labelsize)
+ax.set_title("Strong scaling", fontsize = titlesize)
+
+ax.legend(fontsize = legendsize)
+
+plt.show()
+fig.savefig(plotname, bbox_inches='tight', facecolor=fig.get_facecolor())
